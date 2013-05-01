6e670b8ab4a66baeec450d080093393844a2d2f2
ackzell <ackzell@gmail.com>
2013-04-30 22:51:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Meteor calendar app.
+====================
+
+This is my first attempt to build an app on Meteor.
---
+++ client/views/welcome.js
@@ -0,0 +1,7 @@
+Template.welcome.events({
+  'click input' : function () {
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+      console.log("You pressed the button");
+  }
+});
